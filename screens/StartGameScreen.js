c7ef3d48089e6821593e3a363c52d3c932700a98
mkoenke <mkoenke@sas.upenn.edu>
2021-01-10 06:11:42 -0500

    card rendering

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,61 @@
+import React from "react"
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+import Card from "../components/Card"
+
+const StartGameScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <Card style={styles.inputContainer}>
+        {/* <View style={styles.inputContainer}> */}
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color="#c717fc" />
+          </View>
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color="#f7287b" />
+          </View>
+        </View>
+        {/* </View> */}
+      </Card>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+    // shadowColor: "black",
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowRadius: 6,
+    // shadowOpacity: 0.26,
+    // backgroundColor: "white",
+    // elevation: 8, //for android
+    // padding: 20,
+    // borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  button: {
+    width: 100,
+  },
+})
+
+export default StartGameScreen
